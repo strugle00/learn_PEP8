94ccc154361dca4e5a4d150be0f4b306e7173485
strugle00 <kirilll8181@gmail.com>
2026-05-20 13:07:15 +0700

    task_5.py

---
+++ task_5.py
@@ -0,0 +1,17 @@
+def check_values(a, b):
+    if a is True and b is False:
+        return True
+    elif a is None or b is None:
+        return None
+    else:
+        if a > b:
+            return 1
+        elif a < b:
+            return -1
+        elif a == b:
+            return 0
+
+
+flag = True
+while flag:
+    flag = False
