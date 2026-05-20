d47ef0fefa96aabeb35a447d8a4fa7aa653deded
strugle00 <kirilll8181@gmail.com>
2026-05-20 12:58:53 +0700

    task_2.py

---
+++ task_2.py
@@ -0,0 +1,15 @@
+class UserData:
+    def __init__(self, name, age):
+        self.user_name = name
+        self.age = age
+
+    def print_info(self):
+        print(f"User: {self.user_name}, Age: {self.age}")
+
+
+def process_data(data_list):
+    result = []
+    for data in data_list:
+        if data.age > 18:
+            result.append(data.user_name.upper())
+    return result
